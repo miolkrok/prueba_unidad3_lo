39939c7ba7732df81e6f102aa68c8b6e0223ec34
Luis Ortiz <luchoaam97@gmail.com>
2022-03-14 19:20:46 -0500

    primer commit parte dos

---
+++ src/test/java/ec/edu/uce/PruebaUnidad3LoApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.uce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaUnidad3LoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
